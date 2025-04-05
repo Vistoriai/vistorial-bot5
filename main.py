ec582841abea5606638af7d92194e9abd799a89c
Vistoriai <raphaelcarneiro1801@gmail.com>
2025-04-05 18:35:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
-def home():
-    return {"status": "VistoriAI pronto!"}
+async def home():
+    return JSONResponse({"status": "API Online - VistoriAI"})
+
+# ESSA ROTA É OBRIGATÓRIA PARA O VERCEL
+@app.get("/api/status")
+async def vercel_check():
+    return JSONResponse({"deploy": "success"})
